64e2842d6340bb36146e45571660ae5673322a83
Ebube <chukwumdiebubeojinta@gmail.com>
2023-06-09 23:42:08 +0100

    .

---
+++ LessonJs1/main.js
@@ -0,0 +1,8 @@
+// Creating variables for summoning elements
+const btn = document.getElementById("btn")
+const text = document.getElementById("text")
+
+// adding event listener
+btn.addEventListener("click", () => {
+    console.log("first")
+})
